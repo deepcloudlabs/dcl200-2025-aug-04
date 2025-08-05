0d7a37ae083139aa79e37c07f07e299cfd93b8d3
Binnur KURT <deepcloudlabs@gmail.com>
2025-08-05 12:17:22 +0300

    initial commit

---
+++ performance-affect-os-cpu/src/com/example/Exercise07.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Exercise07 {
+	private static Map<String,Integer> numbers = new HashMap<>();
+	private static Map<String,Integer> numbers2 = new ConcurrentHashMap<>();
+	public static void main(String[] args) {
+		// Thread-safety + NOT multi-core efficient
+		numbers = Collections.synchronizedMap(numbers);
+        // ConcurrentHashMap: TS + multi-core efficient
+		// 10 Regions
+	}
+
+}
